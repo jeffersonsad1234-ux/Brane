7e923ac37d3e5299d8cd220f035a267a256953a2
Jefferson <jeffersonsad1234@gmail.com>
2026-05-26 23:04:37 +0200

    fix(engine): remove SSAO and autoClear from EffectsComposer to stop context loss

--- branpy-game-engine/src/editor/Effects.jsx
+++ branpy-game-engine/src/editor/Effects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { EffectComposer, Bloom, SSAO, BrightnessContrast, HueSaturation, Vignette, Noise, ToneMapping, ChromaticAberration } from "@react-three/postprocessing";
+import { EffectComposer, Bloom, Vignette, Noise, ChromaticAberration } from "@react-three/postprocessing";
 import { useEditorStore } from "@store/editorStore";
 import { BlendFunction } from "postprocessing";
 
@@ -7,37 +7,24 @@ export default function Effects() {
   const env = useEditorStore((s) => s.scene.environment);
   if (!env) return null;
 
-  const { bloom, ssao, colorGrading, vignette, chromaticAberration, bloomIntensity, bloomThreshold } = env;
+  const { bloom, chromaticAberration, vignette, bloomIntensity, bloomThreshold } = env;
 
   return (
-    <EffectComposer multisampling={4} autoClear={false}>
+    <EffectComposer multisampling={0}>
       {bloom && (
         <Bloom intensity={bloomIntensity ?? 0.3} luminanceThreshold={bloomThreshold ?? 0.1}
           luminanceSmoothing={0.08} mipmapBlur />
       )}
 
-      {ssao && (
-        <SSAO samples={12} radius={0.35} intensity={1.5} luminanceInfluence={0.4}
-          color="rgba(0,0,0,0.8)" blendFunction={BlendFunction.MULTIPLY} />
-      )}
-
-      {colorGrading && (
-        <>
-          <BrightnessContrast brightness={0} contrast={0.1} />
-          <HueSaturation hue={0} saturation={-0.05} />
-          <ToneMapping blendFunction={BlendFunction.NORMAL} opacity={0.6} />
-        </>
-      )}
-
       {chromaticAberration && (
-        <ChromaticAberration offset={[0.003, 0.001]} radialModulation={false} blendFunction={BlendFunction.NORMAL} />
+        <ChromaticAberration offset={[0.002, 0.001]} blendFunction={BlendFunction.NORMAL} />
       )}
 
       {vignette && (
         <Vignette offset={0.3} darkness={0.5} eskil={false} blendFunction={BlendFunction.NORMAL} />
       )}
 
-      <Noise opacity={0.012} blendFunction={BlendFunction.SCREEN} />
+      <Noise opacity={0.01} blendFunction={BlendFunction.SCREEN} />
     </EffectComposer>
   );
 }
